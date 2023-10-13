9473d0e11d69b4533383242fc75385cb2cb4aa6a
maksyuki <maksyuki@126.com>
2023-10-13 21:22:32 +0800

    feat: rewrite all the module

--- rtl/apb4_timer.sv
+++ rtl/apb4_timer.sv
@@ -1,15 +1,3 @@
-// Copyright 2015 ETH Zurich and University of Bologna.
-// Copyright and related rights are licensed under the Solderpad Hardware
-// License, Version 0.51 (the “License”); you may not use this file except in
-// compliance with the License.  You may obtain a copy of the License at
-// http://solderpad.org/licenses/SHL-0.51. Unless required by applicable law
-// or agreed to in writing, software, hardware and materials distributed under
-// this License is distributed on an “AS IS” BASIS, WITHOUT WARRANTIES OR
-// CONDITIONS OF ANY KIND, either express or implied. See the License for the
-// specific language governing permissions and limitations under the License.
-//
-// -- Adaptable modifications are redistributed under compatible License --
-//
 // Copyright (c) 2023 Beijing Institute of Open Source Chip
 // timer is licensed under Mulan PSL v2.
 // You can use this software according to the terms and conditions of the Mulan PSL v2.
@@ -20,133 +8,139 @@
 // MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
 // See the Mulan PSL v2 for more details.
 
-`define REGS_MAX_ADR 2'd2
-`define REGS_MAX_IDX 'd2
-`define REG_TIMER 2'b00
-`define REG_TIMER_CTRL 2'b01
-`define REG_CMP 2'b10
-`define PRESCALER_STARTBIT 'd3
-`define PRESCALER_STOPBIT 'd5
-`define ENABLE_BIT 'd0
+// verilog_format: off
+`define TIM_CTRL 4'b0000 //BASEADDR+0x00
+`define TIM_PSCR 4'b0001 //BASEADDR+0x04
+`define TIM_CNT  4'b0010 //BASEADDR+0x08
+`define TIM_CMP  4'b0011 //BASEADDR+0x0C
+// verilog_format: on
 
-module apb4_timer #(
-    parameter TIM_NUM        = 2
-) (
-    apb4_if                       apb4,
-    logic  [(TIM_NUM * 2) - 1:0] irq_o
+/* register mapping
+ * TIM_CTRL:
+ * BITS:   | 31:3 | 2  | 1    | 0     |
+ * FIELDS: | RES  | EN | OVIE | OVIF  |
+ * PERMS:  | NONE | RW | RW   | RC_W0 |
+ * ------------------------------------
+ * TIM_PSCR:
+ * BITS:   | 31:20 | 19:0 |
+ * FIELDS: | RES   | PSCR |
+ * PERMS:  | NONE  | W    |
+ * ------------------------------------
+ * TIM_CNT:
+ * BITS:   | 31:0 |
+ * FIELDS: | CNT  |
+ * PERMS:  | RW   |
+ * ------------------------------------
+ * TIM_CMP:
+ * BITS:   | 31:0 |
+ * FIELDS: | CMP  |
+ * PERMS:  | RW   |
+*/
+module apb4_timer (
+    // verilog_format: off
+    apb4_if.slave apb4,
+    // verilog_format: on
+    output logic irq_o
 );
 
-  logic [          TIM_NUM-1:0]       psel;
-  logic [          TIM_NUM-1:0]       pready;
-  logic [          TIM_NUM-1:0]       pslverr;
-  logic [          TIM_NUM-1:0][31:0] prdata;
-  logic [$clog2(TIM_NUM) - 1:0]       addr;
+  logic [3:0] s_apb_addr;
+  logic [31:0] s_tim_ctrl_d, s_tim_ctrl_q;
+  logic [31:0] s_tim_pscr_d, s_tim_pscr_q;
+  logic [31:0] s_tim_cnt_d, s_tim_cnt_q;
+  logic [31:0] s_tim_cmp_d, s_tim_cmp_q;
+  logic s_valid, s_ready, s_done, s_tc_clk;
+  logic s_apb4_wr_hdshk, s_apb4_rd_hdshk, s_normal_mode;
+  logic s_ov_irq;
 
-  assign addr = apb.paddr[$clog2(TIM_NUM)+`REGS_MAX_ADR+1:`REGS_MAX_ADR+2];
+  assign s_apb_addr      = apb.paddr[5:2];
+  assign s_apb4_wr_hdshk = apb.psel && apb.penable && apb.pwrite;
+  assign s_apb4_rd_hdshk = apb.psel && apb.penable && (~apb.pwrite);
+  assign s_normal_mode   = s_tim_ctrl_q[2] & s_done;
+  assign s_ov_irq        = s_tim_ctrl_q[1] & s_tim_ctrl_q[0];
+  assign irq_o           = s_ov_irq;
 
   always_comb begin
-    psel       = '0;
-    psel[addr] = apb.psel;
-  end
-
-  always_comb begin
-    if (psel != '0) begin
-      apb.prdata  = prdata[addr];
-      apb.pready  = pready[addr];
-      apb.pslverr = pslverr[addr];
-    end else begin
-      apb.prdata  = '0;
-      apb.pready  = 1'd1;
-      apb.pslverr = 1'd0;
+    s_tim_pscr_d = s_tim_pscr_q;
+    if (s_apb4_wr_hdshk && s_apb_addr == `TIM_DIV) begin
+      s_tim_pscr_d = apb4.pwdata < 2 ? 32'd2 : abp4.pwdata;
     end
   end
 
-  genvar i;
-  generate
-    for (i = 0; i < TIM_NUM; i++) begin : TIMER_GEN
-      timer u_timer (apb.);
+  dffr #(32) u_tim_pscr_dffr (
+      .clk_i  (apb4.hclk),
+      .rst_n_i(apb4.hresetn),
+      .dat_i  (s_tim_pscr_d),
+      .dat_o  (s_tim_pscr_q)
+  );
+
+  assign s_valid = s_apb4_wr_hdshk && s_apb_addr == `TIM_PSCR && s_done;
+  clk_int_even_div_simple u_clk_int_even_div_simple (
+      .clk_i      (apb4.hclk),
+      .rst_n_i    (apb4.hresetn),
+      .div_i      (s_tim_pscr_q),
+      .div_valid_i(s_valid),
+      .div_ready_o(s_ready),
+      .div_done_o (s_done),
+      .clk_o      (s_tc_clk)
+  );
+
+  always_comb begin
+    s_tim_cnt_d = s_tim_cnt_q;
+    if (s_normal_mode) begin
+      if (s_tim_cnt_q == s_tim_cmp_q) begin
+        s_tim_cnt_d = '0;
+      end else begin
+        s_tim_cnt_d = s_tim_cnt_q + 1'b1;
+      end
     end
-  endgenerate
-endmodule
+  end
 
-module timer  (
-           apb_if       apb,
-    output logic  [1:0] irq_o
-);
+  dffr #(32) u_tim_cnt_dffr (
+      s_tc_clk,
+      apb4.hresetn,
+      s_tim_cnt_d,
+      s_tim_cnt_q
+  );
 
-  // APB register interface
-  logic [`REGS_MAX_IDX-1:0] reg_addr;
-  assign reg_addr    = apb.paddr[`REGS_MAX_IDX+2:2];
-  // APB logic: we are always ready to capture the data into our regs
-  // not supporting transfare failure
+  always_comb begin
+    s_tim_ctrl_d = s_tim_ctrl_q;
+    if (s_apb4_wr_hdshk && s_apb_addr == `TIM_CTRL) begin
+      s_tim_ctrl_d = apb4.pwdata;
+    end else if (s_normal_mode) begin
+        if(s_tim_cnt_q == s_tim_cmp_q) begin
+          s_tim_ctrl_d[0] = 1'b1;
+        end
+    end
+  end
+
+  dffr #(32) u_tim_ctrl_dffr (
+      apb4.hclk,
+      apb4.hresetn,
+      s_tim_ctrl_d,
+      s_tim_ctrl_q
+  );
+
+  assign s_tim_cmp_d = (s_apb4_wr_hdshk && s_apb_addr == `TIM_CMP) ? apb4.pwdata : s_tim_cmp_q;
+  dffr #(32) u_tim_cmp_dffr (
+      apb4.hclk,
+      apb4.hresetn,
+      s_tim_cmp_d,
+      s_tim_cmp_q
+  );
+
+  always_comb begin
+    apb.prdata = '0;
+    if (s_apb4_rd_hdshk) begin
+      unique case (s_apb_addr)
+        `TIM_CTRL: apb.prdata = s_tim_ctrl_q;
+        `TIM_CNT:  apb.prdata = s_tim_cnt_q;
+        `TIM_CMP:  apb.prdata = s_tim_cmp_q;
+      endcase
+    end
+  end
+
   assign apb.pready  = 1'b1;
   assign apb.pslverr = 1'b0;
-  // registers
-  logic [0:`REGS_MAX_IDX][31:0] regs_q;
-  logic [0:`REGS_MAX_IDX][31:0] regs_n;
-  logic [           31:0]       cycle_counter_n;
-  logic [           31:0]       cycle_counter_q;
-  logic [            2:0]       prescaler_int;
 
-  //irq
-  always_comb begin
-    irq_o = 2'b00;
-    // overlow irq
-    if (regs_q[`REG_TIMER] == 32'hFFFF_FFFF) irq_o[0] = 1'b1;
-    // compare match irq if compare reg ist set
-    if (regs_q[`REG_CMP] != 'b0 && regs_q[`REG_TIMER] == regs_q[`REG_CMP]) irq_o[1] = 1'b1;
-
-  end
-
-  assign prescaler_int = regs_q[`REG_TIMER_CTRL][`PRESCALER_STOPBIT:`PRESCALER_STARTBIT];
-  // register write logic
-  always_comb begin
-    regs_n          = regs_q;
-    cycle_counter_n = cycle_counter_q + 1;
-
-    // reset timer after cmp or overflow
-    if (irq_o[0] == 1'b1 || irq_o[1] == 1'b1) regs_n[`REG_TIMER] = 'b0;
-    else if(regs_q[`REG_TIMER_CTRL][`ENABLE_BIT] && prescaler_int != 'b0 && prescaler_int == cycle_counter_q) // prescaler
-      regs_n[`REG_TIMER] = regs_q[`REG_TIMER] + 1;  //prescaler mode
-    else if (regs_q[`REG_TIMER_CTRL][`ENABLE_BIT] && prescaler_int == 'b0)  // normal count mode
-      regs_n[`REG_TIMER] = regs_q[`REG_TIMER] + 1;
-
-    // reset prescaler cycle counter
-    if (cycle_counter_q >= regs_q[`REG_TIMER_CTRL]) cycle_counter_n = 32'b0;
-
-    // written from APB bus - gets priority
-    if (apb.psel && apb.penable && apb.pwrite) begin
-      unique case (reg_addr)
-        `REG_TIMER:      regs_n[`REG_TIMER] = apb.pwdata;
-        `REG_TIMER_CTRL: regs_n[`REG_TIMER_CTRL] = apb.pwdata;
-        `REG_CMP: begin
-          regs_n[`REG_CMP]   = apb.pwdata;
-          regs_n[`REG_TIMER] = 32'b0;  // reset timer if compare register is written
-        end
-      endcase
-    end
-  end
-
-  // APB register read logic
-  always_comb begin
-    apb.prdata = 'd0;
-    if (apb.psel && apb.penable && !apb.pwrite) begin
-      unique case (reg_addr)
-        `REG_TIMER:      apb.prdata = regs_q[`REG_TIMER];
-        `REG_TIMER_CTRL: apb.prdata = regs_q[`REG_TIMER_CTRL];
-        `REG_CMP:        apb.prdata = regs_q[`REG_CMP];
-      endcase
-    end
-  end
-
-  always_ff @(posedge apb.pclk, negedge apb.presetn) begin
-    if (~apb.presetn) begin
-      regs_q          <= '{default: 32'd0};
-      cycle_counter_q <= 32'd0;
-    end else begin
-      regs_q          <= regs_n;
-      cycle_counter_q <= cycle_counter_n;
-    end
-  end
 endmodule
 
