0208f31d71d5c975f75b922da15a8680f54c2d31
maksyuki <maksyuki@126.com>
2023-11-19 22:39:37 +0800

    docs: fix some typos

--- tb/test_top.sv
+++ tb/test_top.sv
@@ -1,5 +1,5 @@
 // Copyright (c) 2023 Beijing Institute of Open Source Chip
-// gpio is licensed under Mulan PSL v2.
+// timer is licensed under Mulan PSL v2.
 // You can use this software according to the terms and conditions of the Mulan PSL v2.
 // You may obtain a copy of Mulan PSL v2 at:
 //             http://license.coscl.org.cn/MulanPSL2
