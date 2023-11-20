d0db3f2d7a6136521c740a57875473203b39b805
maksyuki <maksyuki@126.com>
2023-11-20 14:41:14 +0800

    feat: use counter to rewrite all modules

--- rtl/apb4_timer.sv
+++ rtl/apb4_timer.sv
@@ -10,32 +10,41 @@
 
 `include "register.sv"
 `include "clk_int_div.sv"
+`include "cdc_sync.sv"
+`include "counter.sv"
 `include "timer_define.sv"
 
 module apb4_timer (
-    // verilog_format: off
     apb4_if.slave apb4,
-    output logic irq_o
-    // verilog_format: on
+    timer_if.dut  timer
 );
 
   logic [3:0] s_apb4_addr;
   logic [`TIM_CTRL_WIDTH-1:0] s_tim_ctrl_d, s_tim_ctrl_q;
   logic [`TIM_PSCR_WIDTH-1:0] s_tim_pscr_d, s_tim_pscr_q;
-  logic [`TIM_CNT_WIDTH-1:0] s_tim_cnt_d, s_tim_cnt_q;
   logic [`TIM_CMP_WIDTH-1:0] s_tim_cmp_d, s_tim_cmp_q;
-  logic s_valid, s_done, s_tr_clk, s_ov_irq;
+  logic [`TIM_STAT_WIDTH-1:0] s_tim_stat_d, s_tim_stat_q;
+  logic s_valid, s_done, s_inclk, s_tr_clk, s_ov_irq, s_cnt_ov;
   logic s_apb4_wr_hdshk, s_apb4_rd_hdshk, s_normal_mode;
 
-  assign s_apb4_addr     = apb4.paddr[5:2];
+  assign s_apb4_addr = apb4.paddr[5:2];
   assign s_apb4_wr_hdshk = apb4.psel && apb4.penable && apb4.pwrite;
   assign s_apb4_rd_hdshk = apb4.psel && apb4.penable && (~apb4.pwrite);
-  assign apb4.pready     = 1'b1;
-  assign apb4.pslverr    = 1'b0;
+  assign apb4.pready = 1'b1;
+  assign apb4.pslverr = 1'b0;
 
-  assign s_normal_mode   = s_tim_ctrl_q[2] & s_done;
-  assign s_ov_irq        = s_tim_ctrl_q[1] & s_tim_ctrl_q[0];
-  assign irq_o           = s_ov_irq;
+  assign s_tr_clk = s_tim_ctrl_q[1] ? timer.exclk_i : s_inclk;
+  assign s_normal_mode = s_tim_ctrl_q[2] & s_done;
+  assign s_ov_irq = s_tim_ctrl_q[1] & s_tim_stat_q[0];
+  assign timer.irq_o = s_ov_irq;
+
+  assign s_tim_ctrl_d = (s_apb4_wr_hdshk && s_apb4_addr == `TIM_CTRL) ? apb4.pwdata[`TIM_CTRL_WIDTH-1:0]: s_tim_ctrl_q;
+  dffr #(`TIM_CTRL_WIDTH) u_tim_ctrl_dffr (
+      apb4.pclk,
+      apb4.presetn,
+      s_tim_ctrl_d,
+      s_tim_ctrl_q
+  );
 
   always_comb begin
     s_tim_pscr_d = s_tim_pscr_q;
@@ -44,7 +53,7 @@ module apb4_timer (
     end
   end
 
-  dffr #(`TIM_PSCR_WIDTH) u_tim_pscr_dffr (
+  dffrc #(`TIM_PSCR_WIDTH, `PSCR_MIN_VAL) u_tim_pscr_dffr (
       .clk_i  (apb4.pclk),
       .rst_n_i(apb4.presetn),
       .dat_i  (s_tim_pscr_d),
@@ -59,45 +68,19 @@ module apb4_timer (
       .div_valid_i(s_valid),
       .div_ready_o(),
       .div_done_o (s_done),
-      .clk_o      (s_tr_clk)
+      .clk_o      (s_inclk)
   );
 
-  always_comb begin
-    s_tim_cnt_d = s_tim_cnt_q;
-    if (s_normal_mode) begin
-      if (s_tim_cnt_q == s_tim_cmp_q) begin
-        s_tim_cnt_d = '0;
-      end else begin
-        s_tim_cnt_d = s_tim_cnt_q + 1'b1;
-      end
-    end
-  end
-
-  dffr #(`TIM_CNT_WIDTH) u_tim_cnt_dffr (
-      s_tr_clk,
-      apb4.presetn,
-      s_tim_cnt_d,
-      s_tim_cnt_q
-  );
-
-  always_comb begin
-    s_tim_ctrl_d = s_tim_ctrl_q;
-    if (s_apb4_wr_hdshk && s_apb4_addr == `TIM_CTRL) begin
-      s_tim_ctrl_d = apb4.pwdata[`TIM_CTRL_WIDTH-1:0];
-    end else if (s_normal_mode) begin
-      if (s_tim_cnt_q == s_tim_cmp_q) begin
-        s_tim_ctrl_d[0] = 1'b1;
-      end else begin
-        s_tim_ctrl_d[0] = 1'b0; // TODO:
-      end
-    end
-  end
-
-  dffr #(`TIM_CTRL_WIDTH) u_tim_ctrl_dffr (
-      apb4.pclk,
-      apb4.presetn,
-      s_tim_ctrl_d,
-      s_tim_ctrl_q
+  counter #(`TIM_CNT_WIDTH) u_tim_cnt_counter (
+      .clk_i  (s_tr_clk),
+      .rst_n_i(apb4.presetn),
+      .clr_i  (~s_normal_mode),
+      .en_i   (s_normal_mode),
+      .load_i (s_cnt_ov),
+      .down_i (s_tim_ctrl_q[3]),
+      .dat_i  (s_tim_cmp_q),
+      .dat_o  (),
+      .ovf_o  (s_cnt_ov)
   );
 
   assign s_tim_cmp_d = (s_apb4_wr_hdshk && s_apb4_addr == `TIM_CMP) ? apb4.pwdata[`TIM_CMP_WIDTH-1:0] : s_tim_cmp_q;
@@ -108,6 +91,21 @@ module apb4_timer (
       s_tim_cmp_q
   );
 
+  cdc_sync #(2, 1) u_irq_cdc_sync (
+      apb4.pclk,
+      apb4.presetn,
+      s_cnt_ov,
+      s_tim_stat_d[0]
+  );
+
+  // assign s_tim_stat_d = s_tim_stat_q;
+  dffr #(`TIM_STAT_WIDTH) u_tim_stat_dffr (
+      apb4.pclk,
+      apb4.presetn,
+      s_tim_stat_d,
+      s_tim_stat_q
+  );
+
   always_comb begin
     apb4.prdata = '0;
     if (s_apb4_rd_hdshk) begin
@@ -115,6 +113,7 @@ module apb4_timer (
         `TIM_CTRL: apb4.prdata[`TIM_CTRL_WIDTH-1:0] = s_tim_ctrl_q;
         `TIM_PSCR: apb4.prdata[`TIM_PSCR_WIDTH-1:0] = s_tim_pscr_q;
         `TIM_CMP:  apb4.prdata[`TIM_CMP_WIDTH-1:0] = s_tim_cmp_q;
+        `TIM_STAT: apb4.prdata[`TIM_STAT_WIDTH-1:0] = s_tim_stat_q;
         default:   apb4.prdata = '0;
       endcase
     end
--- rtl/timer_define.sv
+++ rtl/timer_define.sv
@@ -13,10 +13,10 @@
 
 /* register mapping
  * TIM_CTRL:
- * BITS:   | 31:3 | 2  | 1    | 0     |
- * FIELDS: | RES  | EN | OVIE | OVIF  |
- * PERMS:  | NONE | RW | RW   | RW    |
- * ------------------------------------
+ * BITS:   | 31:4 | 3    | 2  | 1   | 0    |
+ * FIELDS: | RES  | MODE | EN | ETR | OVIE |
+ * PERMS:  | NONE | RW   | RW | RW  | RW   |
+ * -----------------------------------------
  * TIM_PSCR:
  * BITS:   | 31:20 | 19:0 |
  * FIELDS: | RES   | PSCR |
@@ -31,25 +31,45 @@
  * BITS:   | 31:0 |
  * FIELDS: | CMP  |
  * PERMS:  | RW   |
+ * ------------------------------------
+ * TIM_STAT:
+ * BITS:   | 31:1  | 0    |
+ * FIELDS: | RES   | OVIF |
+ * PERMS:  | NONE  | R    |
+ * ------------------------------------
 */
-
+// 50MHz * 1
+// min: 1/50 = 0.02us = 20ns
+// max: 100/2^20 * 2^32us = 100*2^12us = 409600us = 409.6ms
+// timing range: [20ns ~ 409.6ms]
 // verilog_format: off
 `define TIM_CTRL 4'b0000 // BASEADDR + 0x00
 `define TIM_PSCR 4'b0001 // BASEADDR + 0x04
 `define TIM_CNT  4'b0010 // BASEADDR + 0x08
 `define TIM_CMP  4'b0011 // BASEADDR + 0x0C
+`define TIM_STAT 4'b0100 // BASEADDR + 0x10
 
 `define TIM_CTRL_ADDR {26'b0, `TIM_CTRL, 2'b00}
 `define TIM_PSCR_ADDR {26'b0, `TIM_PSCR, 2'b00}
 `define TIM_CNT_ADDR  {26'b0, `TIM_CNT , 2'b00}
 `define TIM_CMP_ADDR  {26'b0, `TIM_CMP , 2'b00}
+`define TIM_STAT_ADDR {26'b0, `TIM_STAT, 2'b00}
 
-`define TIM_CTRL_WIDTH 3
+`define TIM_CTRL_WIDTH 4
 `define TIM_PSCR_WIDTH 20
 `define TIM_CNT_WIDTH  32
 `define TIM_CMP_WIDTH  32
+`define TIM_STAT_WIDTH 1
 
 `define PSCR_MIN_VAL  {{(`TIM_PSCR_WIDTH-2){1'b0}}, 2'd2}
 // verilog_format: on
 
+interface timer_if ();
+  logic exclk_i;
+  logic capch_i;
+  logic irq_o;
+
+  modport dut(input exclk_i, input capch_i, output irq_o);
+  modport tb(output exclk_i, output capch_i, input irq_o);
+endinterface
 `endif
